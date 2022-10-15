dbbae2a2cf7e38f3750de9348b1877fb0c61cbdb
Ariful491 <arifulhoque091@gmail.com>
2022-10-15 21:48:34 +0600

    user seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Keyword;
 use App\Models\SearchHistory;
+use App\Models\User;
 use Database\Factories\SearchHistoryFactory;
 use Illuminate\Database\Seeder;
 
@@ -17,7 +18,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-          \App\Models\User::factory(3)->create();
+          //\App\Models\User::factory(3)->create();
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
@@ -43,6 +44,11 @@ class DatabaseSeeder extends Seeder
             array('id' => '1','name' => 'Evert','found_times' => '3','created_at' => NULL,'updated_at' => NULL),
             array('id' => '2','name' => 'Alexandro','found_times' => '3','created_at' => NULL,'updated_at' => NULL),
             array('id' => '3','name' => ' RObert','found_times' => '2','created_at' => NULL,'updated_at' => NULL)
+        ]);
+        User::insert([
+            array('id' => '1','name' => ' RObert','email' => 'ratke.cruz@example.org','email_verified_at' => '2022-10-15 20:35:49','password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','remember_token' => 'EKMoPc7gqn','created_at' => '2022-10-15 20:35:49','updated_at' => '2022-10-15 20:35:49'),
+            array('id' => '2','name' => 'Evert Larkin','email' => 'dubuque.milo@example.org','email_verified_at' => '2022-10-15 20:35:49','password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','remember_token' => 'F03D8GvY2K','created_at' => '2022-10-15 20:35:49','updated_at' => '2022-10-15 20:35:49'),
+            array('id' => '3','name' => 'Alexandro Pfeffer','email' => 'vcormier@example.com','email_verified_at' => '2022-10-15 20:35:49','password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi','remember_token' => 'E5fcjwAL7Y','created_at' => '2022-10-15 20:35:50','updated_at' => '2022-10-15 20:35:50')
         ]);
 
     }
